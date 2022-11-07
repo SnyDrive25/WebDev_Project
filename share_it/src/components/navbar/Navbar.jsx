c05fd03d2c66028fc60cdfb6d3ee52838f3cff01
Lucas Kaczmarski <lucaskacz1@gmail.com>
2022-11-07 17:33:25 +0100

    Add Files
    Ajout des fichiers nécessaires pour les différentes parties du site

---
+++ share_it/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.css"
+
+export default function Navbar() {
+    return (
+        <div className="navbarContainer">
+            <div className="navbarLeft"></div>
+                <span className="logo">Share It</span>
+            <div className="navbarCenter"></div>
+            <div className="navbarRight"></div>
+
+        </div>
+    )
+}
