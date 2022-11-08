02d4da413e8938c79e3d36e238d789ce40028f3d
Al3xGROS <al.gros@sfr.fr>
2022-11-08 19:12:57 +0100

    Try_nav_pages

--- share_it/src/App.js
+++ share_it/src/App.js
@@ -1,12 +1,25 @@
-import Home from "./pages/home/Home";
+import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
 import './App.css';
 
 
-function App() {
-  return <Home/>;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
