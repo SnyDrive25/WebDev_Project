565a5676786cf4a4f04799afc3eaea4c5343acb7
SnyDrive ~ Sunil <sunil.goulamhoussen@edu.devinci.fr>
2022-12-05 11:05:14 +0100

    online database test

--- share_it/src/server/server.php
+++ share_it/src/server/server.php
@@ -1,23 +1,22 @@
 <?php
 
-// $servername="https://esilv.olfsoftware.fr";
-// $port=5432;
-// $database="mygrp3";
-// $username="grp3pjhoaw45mr4";
-// $password="7c16YtNhXooP";
-// $charset="utf8mb4";
-
-$servername="localhost";
-$database="shareit";
-$username="root";
-$password="shareit";
+$servername="esilv.olfsoftware.fr";
+$database="mygrp3";
+$username="grp3pjhoaw45mr4";
+$password="7c16YtNhXooP";
 $charset="utf8mb4";
+
+// $servername="localhost";
+// $database="shareit";
+// $username="root";
+// $password="shareit";
+// $charset="utf8mb4";
 
 try {
     $dsn="mysql:host=$servername;dbname=$database;charset=$charset";
     $pdo=new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Origin: *");
     return $pdo;
 } catch(PDOException $e) {
     echo "Connection failed: ". $e->getMessage();
